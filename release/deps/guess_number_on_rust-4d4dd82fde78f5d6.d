e57e035ead6cc836047f17e9522377a368d7bb67
Bean <82229220+BeanBeing@users.noreply.github.com>
2022-07-08 19:34:57 -0400

    Delete release directory

--- release/deps/guess_number_on_rust-4d4dd82fde78f5d6.d
+++
@@ -1,5 +0,0 @@
-/home/bean/Dev/Rust/guess-number-on-rust/target/release/deps/guess_number_on_rust-4d4dd82fde78f5d6: src/main.rs
-
-/home/bean/Dev/Rust/guess-number-on-rust/target/release/deps/guess_number_on_rust-4d4dd82fde78f5d6.d: src/main.rs
-
-src/main.rs:
--- release/deps/rand_chacha-1e7c62da9585b268.d
+++
@@ -1,9 +0,0 @@
-/home/bean/Dev/Rust/guess-number-on-rust/target/release/deps/rand_chacha-1e7c62da9585b268.rmeta: /home/bean/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/bean/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/bean/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/home/bean/Dev/Rust/guess-number-on-rust/target/release/deps/librand_chacha-1e7c62da9585b268.rlib: /home/bean/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/bean/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/bean/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/home/bean/Dev/Rust/guess-number-on-rust/target/release/deps/rand_chacha-1e7c62da9585b268.d: /home/bean/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/bean/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/bean/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/home/bean/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs:
-/home/bean/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs:
-/home/bean/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs:
